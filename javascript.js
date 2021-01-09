2e89cff8faf8d77639acf7368b760d366c06304b
LuoYuXuan <reathan@126.com>
2021-01-09 13:25:24 +0800

    Update the framework,correct some mistakes in subwebsites

--- javascript.js
+++ javascript.js
@@ -1,8 +1,35 @@
-let url = "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-03-02/2021-01-01"
-fetch(url)
-    .then(res => res.json())
-    .then(posts => console.log(posts))
-    
-    .catch(function(error){
-        console.log(error)
+$(document).ready(function(){
+    var url = "https://api.apify.com/v2/datasets/suHgi59tSfu02VsRO/items?format=json&clean=1"
+
+    $.getJSON(url,function(data){
+        console.log(data);
+
+        var total_active,total_recovered,total_deaths,total_confirmed;
+
+        var active = [];
+        var confirmed = [];
+        var recovered = [];
+        var deaths = [];
+
+        $.each(data,function(id,obj){
+            active.push(obj.activeCases);
+            confirmed.push(obj.confirmed);
+            recovered.push(obj.recovered);
+            deaths.push(obj.deaths);
+        })
+
+        console.log(active);
+
+        total_active =data[data.length-1].activeCases;
+        total_confirmed = data[data.length-1].infected;
+        total_recovered = data[data.length-1].recovered;
+        total_deaths = data[data.length-1].deceased;
+        $("#active").append(total_active);
+        $("#confirmed").append(total_confirmed);
+        $("#recovered").append(total_recovered);
+        $("#deaths").append(total_deaths);
     })
+
+
+
+})
